462b04006ecb58e526f314ed97291f980cef3485
Kakha <kakhachankseliani@gmail.com>
2024-01-08 14:24:35 +0400

    step-two: simple command for line count

--- 00-wc-tool/index.js
+++ 00-wc-tool/index.js
@@ -7,14 +7,35 @@ const command = process.argv[2];
 const fileName = process.argv[3];
 // check if command was provided
 if (!commands.includes(command)) {
-	console.log("Command not found.\nSecond argument must be a command option!");
+	console.log(
+		`\x1b[1mCommand not found. 
+Second argument must be a command option!\x1b[0m
+Aviable commands: 
+-c - count bytes, 
+-l - outputs number of lines,
+-w - outputs number of words`
+	);
 	process.exit();
 }
 // create file content constant
 const fileContent = fs.readFileSync(fileName, "utf-8");
+if (command === "-c") {
+	// create a function that count byteLength of the file
+	const byteLength = Buffer.byteLength(fileContent);
 
-// create a function that count byteLength of the file
-const byteLength = Buffer.byteLength(fileContent);
-
-// console the thing
-console.log(byteLength);
+	// console the thing
+	console.log(byteLength);
+}
+// create a function for commanw -l
+else if (command === "-l") {
+	// split text by new line
+	let lines = fileContent.split("\n");
+	// check if there is no new line at the end of the file
+	if (lines[lines.length - 1] === "") {
+		lines = lines.length - 1;
+	} else {
+		lines = lines.length;
+	}
+	// console the thing
+	console.log(lines);
+}
