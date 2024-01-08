58cb7c36efedfe1b9c53991109434c2258390db5
Kakha <kakhachankseliani@gmail.com>
2024-01-08 12:24:04 +0400

    step-one: added simple logic for bytes count.

--- 00-wc-tool/index.js
+++ 00-wc-tool/index.js
@@ -1,9 +1,20 @@
 const fs = require("fs");
-
+const commands = ["-c", "-w", "-l"];
 console.log(process.argv[2]);
+// read and assign the command name
+const command = process.argv[2];
+// read adn assign the file name
+const fileName = process.argv[3];
+// check if command was provided
+if (!commands.includes(command)) {
+	console.log("Command not found.\nSecond argument must be a command option!");
+	process.exit();
+}
+// create file content constant
+const fileContent = fs.readFileSync(fileName, "utf-8");
 
-const fileName = process.argv[2];
+// create a function that count byteLength of the file
+const byteLength = Buffer.byteLength(fileContent);
 
-fs.readFile(fileName, "utf8", (err, data) => {
-	console.log(data);
-});
+// console the thing
+console.log(byteLength);
