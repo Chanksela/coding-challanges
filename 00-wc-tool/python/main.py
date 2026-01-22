5597d26e763bafa23c0ba2479ff3555d86e8635f
chanksela <kakhachankseliani@gmail.com>
2026-01-22 19:37:56 +0400

    refactor and step four

--- 00-wc-tool/python/main.py
+++ 00-wc-tool/python/main.py
@@ -10,32 +10,62 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-c", "--count", action="store_true", help="byte count")
 parser.add_argument("-l", "--line", action="store_true", help="line count")
 parser.add_argument("-w", "--word", action="store_true", help="word count")
+parser.add_argument("-m", "--character", action="store_true", help="character count")
 
 # Positional arguments
 parser.add_argument("file", help="File to process")
 
 args = parser.parse_args()
 
-if args.count:
+
+def countBytes():
     try:
         with open(args.file, "rb") as f:
             byte_count = len(f.read())
-            print(byte_count, args.file)
     except FileNotFoundError:
         print(f"No file with name {args.file}")
 
-if args.line:
+    return byte_count
+
+
+def countLines():
     try:
         with open(args.file, "r") as f:
             line_count = len(f.readlines())
-            print(line_count, args.file)
     except FileNotFoundError:
         print(f"No file with name {args.file}")
+    return line_count
 
-if args.word:
+
+def countWords():
     try:
         with open(args.file, "r") as f:
             word_count = len(f.read().split())
-            print(word_count, args.file)
     except FileNotFoundError:
         print(print(f"No file with name {args.file}"))
+    return word_count
+
+
+def countCharacters():
+    try:
+        with open(args.file, "rb") as f:
+            character_count = len(f.read().decode("utf-8"))
+    except FileNotFoundError:
+        print(f"No file with name {args.file}")
+    except UnicodeDecodeError:
+        print(f"Error decoding {args.file}")
+    return character_count
+
+
+if args.count or args.line or args.word:
+    if args.count:
+
+        print(countBytes(), args.file)
+    if args.line:
+        print(countLines(), args.file)
+    if args.word:
+        print(countWords(), args.file)
+    if args.character:
+        print(countCharacters(), args.file)
+else:
+    print(countBytes(), countLines(), countWords(), args.file)
